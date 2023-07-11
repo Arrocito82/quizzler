c5a8f65b00fb89d999c27a61587e9e85907d5942
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-10 19:32:28 -0600

    Review Mode

--- lib/main.dart
+++ lib/main.dart
@@ -61,21 +61,38 @@ class _QuizzlerPageState extends State<QuizzlerPage> {
   int rightAnswersCounter = 0;
   bool reviewMode = false;
 
-  bool areQuestionsRemaining() {
-    return wrongAnswersCounter + rightAnswersCounter < quizBrain.count();
-  }
-
   void restartQuiz() {
     Navigator.pop(context);
     setState(() {
       wrongAnswersCounter = 0;
       rightAnswersCounter = 0;
       quizBrain.restart();
+      reviewMode = false;
     });
   }
 
-  void previousQuestion() => quizBrain.previousQuestion();
-  void nextQuestion() => quizBrain.nextQuestion();
+  void reviewQuiz() {
+    Navigator.pop(context);
+    setState(() {
+      reviewMode = true;
+    });
+  }
+
+  bool areQuestionsRemaining() {
+    return wrongAnswersCounter + rightAnswersCounter < quizBrain.count();
+  }
+
+  void previousQuestion() {
+    setState(() {
+      quizBrain.previousQuestion();
+    });
+  }
+
+  void nextQuestion() {
+    setState(() {
+      quizBrain.nextQuestion();
+    });
+  }
 
   void checkAnswer(bool userPickedAnswer) {
     setState(() {
@@ -112,8 +129,7 @@ class _QuizzlerPageState extends State<QuizzlerPage> {
 
   void displayOptions() {
     List<DialogButton> buttons = [];
-    //TODO: if all questions are answered then you can review the questions
-    if (!areQuestionsRemaining()) {
+    if (!areQuestionsRemaining() && reviewMode == false) {
       buttons.add(getReviewButton());
     }
     buttons.add(getRestartButton());
@@ -135,7 +151,9 @@ class _QuizzlerPageState extends State<QuizzlerPage> {
 
     String description =
         "Oops! Try again! \nRestart the quiz and give it another shot. \nYou've got this!";
-    buttons.add(getReviewButton());
+    if (reviewMode == false) {
+      buttons.add(getReviewButton());
+    }
     buttons.add(getRestartButton());
     if (rightAnswersCounter / quizBrain.count() >= 0.7) {
       resultIcon = Icon(
@@ -154,9 +172,12 @@ class _QuizzlerPageState extends State<QuizzlerPage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                resultIcon,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: resultIcon,
+                ),
                 Text(
                   "$rightAnswersCounter/${quizBrain.count()}",
                   style: TextStyle(
@@ -207,161 +228,198 @@ class _QuizzlerPageState extends State<QuizzlerPage> {
       return Colors.green.shade500;
     }
 
+    final buttonTextStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+      fontFamily: "Poppins",
+    );
+    // Question Section
+    List<Expanded> quizContent = [
+      Expanded(
+          child: Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 8.0),
+              child: TextButton(
+                onPressed: () {
+                  displayOptions();
+                },
+                child: Icon(
+                  Icons.more_vert,
+                  size: 20.0,
+                  weight: 16.0,
+                  color: Colors.white,
+                ),
+              ))),
+      Expanded(
+        flex: 4,
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 0),
+            child: Text(
+              quizBrain.getQuestionText(),
+              style: buttonTextStyle,
+            ),
+          ),
+        ),
+      ),
+    ];
+
+    //Buttons Section
+    if (reviewMode == false) {
+      quizContent.addAll(<Expanded>[
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                checkAnswer(true);
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.resolveWith(getColorSuccessButton),
+              ),
+              child: Text(
+                "True",
+                style: buttonTextStyle,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                checkAnswer(false);
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.resolveWith(getColorDangerButton),
+              ),
+              child: Text(
+                "False",
+                style: buttonTextStyle,
+              ),
+            ),
+          ),
+        )
+      ]);
+    } else {
+      //When review Mode it shows the answer
+      quizContent.addAll(<Expanded>[
+        Expanded(
+            child: Text(
+          "Answer: ${quizBrain.getQuestionAnswer() == true ? "True" : "False"}",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontFamily: "Poppins",
+              fontStyle: FontStyle.italic),
+          textAlign: TextAlign.center,
+        )),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  previousQuestion();
+                },
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 50.0,
+                  color: Colors.white,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  nextQuestion();
+                },
+                child: Icon(
+                  Icons.arrow_forward,
+                  size: 50.0,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        )
+      ]);
+    }
+
+    // Scores Section
+    quizContent.add(Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Scores",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Poppins",
+                  color: Colors.white),
+            ),
+            Row(
+              children: [
+                Text(
+                  '$rightAnswersCounter',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w900),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.green,
+                    weight: 12.0,
+                    size: 30.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  '$wrongAnswersCounter',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w900),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.redAccent,
+                    weight: 12.0,
+                    size: 30.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
     return SafeArea(
       child: Container(
         color: const Color.fromRGBO(1, 48, 70, 1.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-                child: Container(
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.only(right: 8.0),
-                    child: TextButton(
-                      onPressed: () {
-                        displayOptions();
-                      },
-                      child: Icon(
-                        Icons.more_vert,
-                        size: 20.0,
-                        weight: 16.0,
-                        color: Colors.white,
-                      ),
-                    ))),
-            Expanded(
-              flex: 4,
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 0),
-                  child: Text(
-                    quizBrain.getQuestionText(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                      fontFamily: "Poppings",
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton(
-                  onPressed: () {
-                    checkAnswer(true);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith(
-                        getColorSuccessButton),
-                  ),
-                  child: const Text(
-                    "True",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontFamily: "Poppins",
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton(
-                  onPressed: () {
-                    checkAnswer(false);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.resolveWith(getColorDangerButton),
-                  ),
-                  child: const Text(
-                    "False",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontFamily: "Poppins",
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            // Divider(
-            //   color: Colors.white,
-            //   thickness: 2,
-            // ),
-            Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Scores",
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w700,
-                            fontFamily: "Poppins",
-                            color: Colors.white),
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            '$rightAnswersCounter',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontFamily: "Poppins",
-                                fontWeight: FontWeight.w900),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 12.0),
-                            child: Icon(
-                              Icons.check,
-                              color: Colors.green,
-                              weight: 12.0,
-                              size: 30.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            '$wrongAnswersCounter',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontFamily: "Poppins",
-                                fontWeight: FontWeight.w900),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 12.0),
-                            child: Icon(
-                              Icons.close,
-                              color: Colors.redAccent,
-                              weight: 12.0,
-                              size: 30.0,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ))
-          ],
+          children: quizContent,
         ),
       ),
     );
-  }
-
-  void reviewQuiz() {
-    Navigator.pop(context);
   }
 }
